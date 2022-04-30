2302fc2e60c8eea8e30663f27e4bec7b20636cb1
sarawagi <sarawagi@email.unc.edu>
2022-04-30 11:17:43 -0400

    end point issues

--- server.js
+++ server.js
@@ -79,15 +79,14 @@ app.get('/app/flips/:number/', (req, res) => {
   res.json(rawjson)
 });
 
-app.get('/app/flip/call/heads/', (req, res) => {
-  res.status(200);
-  res.json(flipACoin('heads'));
-});
+app.get('/app/flip/call/heads', (req, res) => {
+    res.status(200).json(flipACoin("heads"))
+})
 
-app.get('/app/flip/call/tails/', (req, res) => {
-  res.status(200);
-  res.json(flipACoin('tails'));
-});
+
+app.get('/app/flip/call/tails', (req, res) => {
+    res.status(200).json(flipACoin("tails"))
+})
 
 app.use(function(req, res){
   res.status(404).send('404 NOT FOUND')
